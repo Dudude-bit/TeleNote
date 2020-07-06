96c09a8f6030e9066376ef0574e453ac942f2b29
Kirill Inyakin <kirillinyakin@yandex.ru>
2020-07-06 15:29:20 +0300

    fixes

--- db.py
+++ db.py
@@ -5,11 +5,13 @@ def create_connection():
     connection = None
     try:
         connection = mysql.connector.connect(
-            host=,
-            user=user_name,
-            passwd=user_password,
-            db_name=123
+            host='eu-cdbr-west-03.cleardb.net',
+            user='b6df603d349c62',
+            passwd='731e8348',
+            database='heroku_24306a1d51229cb'
         )
     except mysql.connector.Error as e:
         print(e)
     return connection
+
+create_connection()
