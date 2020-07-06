8932d33095342b8e92a992d0233ea6b4920a0b92
Kirill Inyakin <kirillinyakin@yandex.ru>
2020-07-06 14:49:27 +0300

    'fixes'

--- db.py
+++ db.py
@@ -1,12 +1,14 @@
 import mysql.connector
 
+
 def create_connection(host_name, user_name, user_password):
     connection = None
     try:
         connection = mysql.connector.connect(
             host=host_name,
             user=user_name,
-            passwd=user_password
+            passwd=user_password,
+            db_name=123
         )
     except mysql.connector.Error as e:
         print(e)
