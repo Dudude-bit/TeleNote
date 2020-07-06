39e02527f42aaf18f7f8399b44453dab6b162df6
Kirill Inyakin <kirillinyakin@yandex.ru>
2020-07-06 15:32:31 +0300

    fixes

--- db.py
+++ db.py
@@ -1,17 +1,17 @@
-import mysql.connector
+import mysqlclient.connector
 
 
 def create_connection():
     connection = None
     try:
-        connection = mysql.connector.connect(
+        connection = mysqlclient.connector.connect(
             host='eu-cdbr-west-03.cleardb.net',
             user='b6df603d349c62',
             passwd='731e8348',
             database='heroku_24306a1d51229cb'
         )
-    except mysql.connector.Error as e:
+    except mysqlclient.connector.Error as e:
         print(e)
     return connection
 
-create_connection()
+connection = create_connection()
