00a48711081fdf6bff6f83852b352170c10915a1
Kirill Inyakin <kirillinyakin@yandex.ru>
2020-07-06 17:56:35 +0300

    added functionality

--- db.py
+++ db.py
@@ -1,17 +1,34 @@
 import mysql.connector
 
 
-def create_connection():
+def create_connection() :
     connection = None
-    try:
+    try :
         connection = mysql.connector.connect(
             host='eu-cdbr-west-03.cleardb.net',
             user='b6df603d349c62',
             passwd='731e8348',
             database='heroku_24306a1d51229cb'
         )
-    except mysql.connector.Error as e:
+        connection.autocommit = True
+    except mysql.connector.Error as e :
         print(e)
     return connection
 
-connection = create_connection()
+
+def create_note(connection: mysql.connector.connection.MySQLConnection, note_text, user_id) :
+    cursor = connection.cursor()
+    query = f"""
+    INSERT INTO note (note, user_id) VALUES ('{note_text}','{user_id}')
+    """
+    cursor.execute(query)
+
+
+def get_notes(connection: mysql.connector.connection.MySQLConnection, user_id) :
+    cursor = connection.cursor()
+    query = f"""
+    SELECT * FROM note WHERE user_id = {user_id}
+    """
+    cursor.execute(query)
+    notes = cursor.fetchall()
+    return notes
--- main.py
+++ main.py
@@ -6,12 +6,19 @@ TOKEN = '1109252246:AAElFSsGAb7Y_fa_sGbUlnbnKfe-3ZwPH0c'
 
 bot = telebot.TeleBot(token=TOKEN)
 
+step_dict = dict()
+
+START, ADD_NOTE = range(2)
+
+CONNECTION = db.create_connection()
+
 
 @bot.message_handler(commands=['start'])
 def handling_start(message: telebot.types.Message) :
     kb = telebot.types.InlineKeyboardMarkup()
     btn1 = telebot.types.InlineKeyboardButton(text='Добавить заметку', callback_data='add_note')
-    kb.add(btn1)
+    btn2 = telebot.types.InlineKeyboardButton(text='Показать Все заметки', callback_data='show_note')
+    kb.add(btn1, btn2)
     bot.send_message(message.chat.id,
                      'Здравствуйте, это простой бот, который Вам поможет сохранять заметки. Для более подробной информации введите команду /help',
                      reply_markup=kb)
@@ -23,6 +30,26 @@ def handling_help(message: telebot.types.Message) :
     Здравствуйте, это простой бот, который Вам будет помогать сохранять заметки, не выходя из Telegram. Бот также поддерживает сохранения из другого чата, для этого просто можно ввести название бота через @, а затем ввести то, что Вы хотите.
     """
     bot.send_message(message.chat.id, text=text)
+
+
+@bot.callback_query_handler(func=lambda x: x.data == 'add_note')
+def adding_note(callback_query: telebot.types.CallbackQuery):
+    step_dict[callback_query.from_user.id] = ADD_NOTE
+    bot.send_message(callback_query.message.chat.id, text='Введите Вашу заметку')
+
+
+@bot.message_handler(content_types=['text'], func=lambda x: step_dict[x.from_user.id] == ADD_NOTE)
+def add_note(message: telebot.types.Message):
+    note_text = message.text
+    user_id = message.from_user.id
+    db.create_note(CONNECTION, note_text, user_id)
+
+@bot.callback_query_handler(func=lambda m:m.data == 'show_note')
+def show_note(callback_query: telebot.types.CallbackQuery):
+    user_id = callback_query.from_user.id
+    notes = db.get_notes(CONNECTION, user_id)
+    for note in notes:
+        bot.send_message(callback_query.message.chat.id, note[1])
 
 
 bot.polling()
