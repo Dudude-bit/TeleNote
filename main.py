ef83bc67529d3b9101fcbeae29a90c3748a91894
Kirill Inyakin <kirillinyakin@yandex.ru>
2020-07-07 15:22:53 +0300

    fixes

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 import telebot
 import db
 
-
 TOKEN = '1109252246:AAElFSsGAb7Y_fa_sGbUlnbnKfe-3ZwPH0c'
 
 bot = telebot.TeleBot(token=TOKEN)
 
 step_dict = dict()
 
-START, ADD_NOTE_TITLE ,ADD_NOTE = range(3)
-
+START, ADD_NOTE_TITLE, ADD_NOTE = range(3)
 
 title_dict = dict()
 
@@ -35,22 +33,23 @@ def handling_help(message: telebot.types.Message) :
     bot.send_message(message.chat.id, text=text)
 
 
-@bot.callback_query_handler(func=lambda x: x.data == 'add_note')
-def adding_note(callback_query: telebot.types.CallbackQuery):
+@bot.callback_query_handler(func=lambda x : x.data == 'add_note')
+def adding_note(callback_query: telebot.types.CallbackQuery) :
     step_dict[callback_query.from_user.id] = ADD_NOTE_TITLE
     bot.send_message(callback_query.message.chat.id, text='Введите название Вашей заметки')
 
 
-@bot.message_handler(content_types=['text'], func=lambda x: step_dict.get(x.from_user.id, START) == ADD_NOTE_TITLE)
-def add_note_title(message: telebot.types.Message):
+@bot.message_handler(content_types=['text'], func=lambda x : step_dict.get(x.from_user.id, START) == ADD_NOTE_TITLE)
+def add_note_title(message: telebot.types.Message) :
     note_title = message.text
     user_id = message.from_user.id
     title_dict[user_id] = note_title
     step_dict[message.from_user.id] = ADD_NOTE
     bot.send_message(message.chat.id, 'Введите теперь Вашу заметку')
 
-@bot.message_handler(content_types=['text'], func=lambda x: step_dict.get(x.from_user.id, START) == ADD_NOTE)
-def add_note(message: telebot.types.Message):
+
+@bot.message_handler(content_types=['text'], func=lambda x : step_dict.get(x.from_user.id, START) == ADD_NOTE)
+def add_note(message: telebot.types.Message) :
     title = title_dict.pop(message.from_user.id)
     text = message.text
     user_id = message.from_user.id
@@ -60,19 +59,20 @@ def add_note(message: telebot.types.Message):
     connection.close()
 
 
-@bot.callback_query_handler(func=lambda m:m.data == 'show_note')
-def show_note(callback_query: telebot.types.CallbackQuery):
+@bot.callback_query_handler(func=lambda m : m.data == 'show_note')
+def show_note(callback_query: telebot.types.CallbackQuery) :
     user_id = callback_query.from_user.id
     connection = db.create_connection()
     notes = db.get_notes(connection, user_id)
     kb = telebot.types.InlineKeyboardMarkup(row_width=1)
-    for note in notes:
+    for note in notes :
         kb.add(telebot.types.InlineKeyboardButton(text=note[3], callback_data=f'note_{note[0]}'))
-    bot.send_message(callback_query.message.chat.id, 'Это все Ваши заметки', reply_markup=kb)
+    bot.edit_message_reply_markup(callback_query.message.chat.id, inline_message_id=callback_query.inline_message_id, reply_markup=kb)
     connection.close()
 
-@bot.callback_query_handler(func=lambda x:x.data.startswith('note'))
-def get_one_note(message):
+
+@bot.callback_query_handler(func=lambda x : x.data.startswith('note'))
+def get_one_note(message) :
     connection = db.create_connection()
     note_id = message.data.replace('note_', '')
     note = db.get_one_note(connection, note_id)[1]
@@ -80,8 +80,8 @@ def get_one_note(message):
     connection.close()
 
 
-@bot.callback_query_handler(func=lambda m:m.data == 'del_notes')
-def delete_all_note(callback_query: telebot.types.CallbackQuery):
+@bot.callback_query_handler(func=lambda m : m.data == 'del_notes')
+def delete_all_note(callback_query: telebot.types.CallbackQuery) :
     user_id = callback_query.from_user.id
     connection = db.create_connection()
     db.delete_all_notes(connection, user_id)
